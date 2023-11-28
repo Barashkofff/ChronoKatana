7d938c922de026cf179b19544e4f12862c198390
bluewater17 <fff.shev1705@gmail.com>
2023-11-28 18:01:31 +0300

    UI
    MainMenu

---
+++ ChronoKatana/Assets/MainMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame(string x)
+    {
+        SceneManager.LoadScene(x);
+    }
+    public void ExitGame()
+    {
+        Debug.Log("конец");
+        Application.Quit();
+    }
+}
