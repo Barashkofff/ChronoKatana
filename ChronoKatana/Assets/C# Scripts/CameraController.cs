23791fbfc6ef7331754ec1075e815173d7a7ffe2
WarLockeR31 <roman.borisenko2016@gmail.com>
2023-12-02 22:16:45 +0300

    Add transit cam animation

--- ChronoKatana/Assets/C# Scripts/CameraController.cs
+++ ChronoKatana/Assets/C# Scripts/CameraController.cs
@@ -4,21 +4,16 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float dumping = 1.5f;
-    public Vector2 offset = new Vector2(2f, 1f);
-    //public bool isLeft;
+    public float dumping;
+    public Vector2 offset;
     private Transform player;
-    //private int lastX;
 
-
-    // Start is called before the first frame update
     void Start()
     {
-        offset = new Vector2(Mathf.Abs(offset.x), offset.y);
         FindPlayer(PlayerController.instance.GetFacing());
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         int currentX = Mathf.RoundToInt(player.position.x);
@@ -41,7 +36,7 @@ public class CameraController : MonoBehaviour
     public void FindPlayer(bool playerIsRight)
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-        
+
         if (playerIsRight )
         {
             transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, transform.position.z);
--- ChronoKatana/Assets/TransitCam.cs
+++ ChronoKatana/Assets/TransitCam.cs
@@ -4,9 +4,45 @@ using UnityEngine;
 
 public class TransitCam : MonoBehaviour
 {
-    public
+    public AnimationCurve curve;
+    public Camera mainCam;
+    public GameObject tabletFullRes;
+
+    private Camera transitCam;
+
+    private Vector3 targetPos;
+    private Vector3 startPos;
+
+    private float startSize;
+    private float targetSize;
+    
+
     void Start()
     {
-        
+        PlayerController.instance.enabled = false;
+        mainCam.GetComponent<CameraController>().FindPlayer(true);
+
+        transitCam = GetComponent<Camera>();
+        startSize = transitCam.orthographicSize;
+        targetSize = mainCam.orthographicSize;
+
+        startPos = transform.position;
+        targetPos = mainCam.transform.position;
+        StartCoroutine(Intro());
+    }
+
+    IEnumerator Intro()
+    {
+        float t = 0;
+        while (t < 1)
+        {
+            transform.position = Vector3.Lerp(startPos, targetPos, t);
+            transitCam.orthographicSize = Mathf.Lerp(startSize, targetSize, t);
+            t += 0.005f;
+            yield return new WaitForEndOfFrame();
+        }
+
+        GetComponent<Camera>().enabled = false;
+        PlayerController.instance.enabled = true;
     }
 }
