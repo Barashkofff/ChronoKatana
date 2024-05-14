e2b76f1008e0992f3343108e77dcb78230fa9a5f
WarLockeR31 <115253196+WarLockeR31@users.noreply.github.com>
2024-05-14 20:29:43 +0300

    Better Dash Animation

--- ChronoKatana/Assets/DashScript.cs
+++ ChronoKatana/Assets/DashScript.cs
@@ -1,20 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using UnityEngine;
 
 public class DashScript : StateMachineBehaviour
 {
+    [SerializeField] private float trans;
     Collider2D collider;
     Transform legs;
+    Transform torso;
+    SpriteRenderer sprite1;
+    SpriteRenderer sprite2;
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         collider = PlayerController.instance.GetComponent<Collider2D>();
+        torso = collider.transform;
         legs = PlayerController.instance.GetLegsTransform();
+        sprite1 = collider.GetComponent<SpriteRenderer>();
+        sprite2 = legs.GetComponent<SpriteRenderer>();
 
         collider.offset += new Vector2(0, 0.125f);
         legs.position += new Vector3(0, 0.125f, 0);
+        torso.position += new Vector3(0, -0.125f, 0);
+        sprite1.color = new Color(1, 1, 1, trans);
+        sprite2.color = new Color(1, 1, 1, trans);
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
@@ -28,6 +39,9 @@ public class DashScript : StateMachineBehaviour
     {
         collider.offset += new Vector2(0, -0.125f);
         legs.position += new Vector3(0, -0.125f, 0);
+        torso.position += new Vector3(0, 0.125f, 0);
+        sprite1.color = new Color(1, 1, 1, 1);
+        sprite2.color = new Color(1, 1, 1, 1);
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
